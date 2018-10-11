911b41d166269608a6e6647c96f1ac9f709f9127
Jeffrey McCracken <jmccracken@c21scheetz.com>
2018-10-11 16:08:39 -0400

    Add handlebars

--- landing-pages/utils/lennar_homes.js
+++ landing-pages/utils/lennar_homes.js
@@ -1,9 +1,53 @@
 // Initialize my carousel
 $(document).ready(function(){
-    console.log('It is linked');
     $('.carousel').slick({
         arrows: false,
         dots: true
     });
 });
+
+// Handlebars setup
+var source = document.getElementById("card-template").innerHTML;
+var cardTemplate = Handlebars.compile(source);
+
+// Property data
+var data = {
+    "neighborhood": {
+        "ashfordPlace": {
+            "id": 1,
+            "property": [
+                {
+                    "imageSrc": "imgs/ashford-place/18753-mcfall-drive.jpeg",
+                    "address": "18753 McFall Drive",
+                    "propertyLink": "#"
+                },
+                {
+                    "imageSrc": "imgs/ashford-place/4515-andover-parkway.jpeg",
+                    "address": "4515 Andover Parkway",
+                    "propertyLink": "#"
+                },
+                {
+                    "imageSrc": "imgs/ashford-place/4515-boyd-place.jpeg",
+                    "address": "4515 Boyd Place",
+                    "propertyLink": "#"
+                }
+            ]
+        },
+        "ashmoor": {
+            "id": 2,
+            "property": [
+                {
+                    "imageSrc": "imgs/ashmoor/3531-moorland-lane.jpeg",
+                    "address": "3531 Moorland Land",
+                    "propertyLink": "#"
+                }
+            ]
+        }
+    }
+};
+
+// Render the data to the template
+var html = cardTemplate(data);
+var propertyContainer = document.getElementById("property-container");
+propertyContainer.innerHTML = html;
 
